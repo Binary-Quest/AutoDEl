8da606a56ee18861241735256b168013415d3e63
Binary-Quest <163310128+Binary-Quest@users.noreply.github.com>
2025-05-10 19:53:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -193,7 +193,7 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Nova_Flix/50")],
+                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/DRXCAPTAINBackUP")],
 
     [
                     InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),
